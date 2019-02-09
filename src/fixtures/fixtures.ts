a29e2a6d56b07c10d3dba2998c5d8000ccaa0084
dawiddominiak <poczta@dawiddominiak.pl>
2019-02-09 03:31:00 +0100

    Fixture change

--- src/fixtures/fixtures.ts
+++ src/fixtures/fixtures.ts
@@ -1,8 +1,8 @@
 import { Cuboid } from "../figures/cuboid.model";
 
 export const fixtures = [
-  new Cuboid(-400, 100, -400, 150, 150, 250),
-  new Cuboid(-400, 100, 0, 150, 150, 400),
-  new Cuboid(0, 100, -400, 150, 300, 100),
-  new Cuboid(250, 100, -200, 150, 100, 200)
+  new Cuboid(-400, 100, -400, 250, 250, 250),
+  new Cuboid(-400, 100, 0, 250, 250, 250),
+  new Cuboid(250, 100, -400, 250, 250, 250),
+  new Cuboid(250, 100, 0, 250, 250, 250)
 ];
